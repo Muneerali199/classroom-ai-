0a3bc9aaf8e3844d7a69c02d004eaad3b113c1a9
Muneer Ali <alimuneerali245@gmail.com>
2025-09-09 18:42:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import DashboardPage from '@/app/[locale]/dashboard/page';
-
-// This component is a workaround to make the dashboard available at /dashboard
-// without being affected by the locale middleware.
-export default function UnlocalizedDashboardPage() {
-  return <DashboardPage />;
-}
