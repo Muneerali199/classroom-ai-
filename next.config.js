3b879e70fcaeadb137e6c381f188f2828ea0019e
Xenonesis <aaktaditya09@gmail.com>
2025-09-15 23:24:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withNextIntl = require('next-intl/plugin')(
+  // This is the default location for the i18n config
+  './src/i18n.ts'
+);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@genkit-ai/googleai', 'genkit'],
@@ -43,4 +48,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
