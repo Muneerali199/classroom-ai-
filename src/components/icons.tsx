d419429f349cc66535eee5dfb0632027e6e5a9c4
Muneerali199 <alimuneerali245@gmail.com>
2025-09-21 21:37:41 +0530

    fix icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import type { SVGProps } from 'react';
 
-export const Logo = ({ className }) => (
-  <svg
-    className={className}
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
+export function Logo(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-    />
-  </svg>
-);
+      {...props}
+    >
+      <path d="M22 10v6M2 10l10-5 10 5-10 5z" />
+      <path d="M6 12v5c3.33 1.67 6.67 1.67 10 0v-5" />
+    </svg>
+  );
+}
