18682183f509970d81d78818242d95f0b09efdc0
Muneerali199 <alimuneerali245@gmail.com>
2025-09-28 02:36:47 +0530

    update: src/routing.ts

--- src/routing.ts
+++ src/routing.ts
@@ -3,7 +3,39 @@ import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'es', 'hi'],
+  // English, Spanish, and all major Indian languages
+  locales: [
+    'en',    // English
+    'es',    // Spanish
+    'hi',    // Hindi
+    'bn',    // Bengali
+    'te',    // Telugu
+    'mr',    // Marathi
+    'ta',    // Tamil
+    'gu',    // Gujarati
+    'ur',    // Urdu
+    'kn',    // Kannada
+    'or',    // Odia
+    'ml',    // Malayalam
+    'pa',    // Punjabi
+    'as',    // Assamese
+    'mai',   // Maithili
+    'mag',   // Magahi
+    'bho',   // Bhojpuri
+    'raj',   // Rajasthani
+    'bpy',   // Bishnupriya
+    'hne',   // Chhattisgarhi
+    'gom',   // Konkani
+    'sa',    // Sanskrit
+    'ne',    // Nepali
+    'ks',    // Kashmiri
+    'sd',    // Sindhi
+    'doi',   // Dogri
+    'mni',   // Manipuri
+    'sat',   // Santali
+    'kok',   // Konkani (Goan)
+    'brx'    // Bodo
+  ],
 
   // Used when no locale matches
   defaultLocale: 'en'
